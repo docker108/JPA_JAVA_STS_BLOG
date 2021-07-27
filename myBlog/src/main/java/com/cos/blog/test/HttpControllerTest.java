d3ca8e6ac8afa60cc63b42cc9963ef56c9468a32
hanjae do <dohanjae@gmail.com>
2021-07-27 16:52:34 +0900

    yml, lombok setting, http test

--- myBlog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ myBlog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -17,6 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = new Member(1, "ssar", "12345", "ssar@nate.com");
+		Member m2 = new Member();	
+		
+		System.out.println(TAG+" getter : "+m.getId());
+		m.setId(5000);
+		System.out.println(TAG+" setter : "+m.getId());
+
+		return "lombok test 완료";
+	}
+	
 	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {// id=1&username=ssar&password=1234&email=ssar@nate.com // MessageConverter(spring boot)
--- myBlog/src/main/java/com/cos/blog/test/Member.java
+++ myBlog/src/main/java/com/cos/blog/test/Member.java
@@ -1,20 +1,46 @@
 package com.cos.blog.test;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 //@Getter
 //@Setter
-@Data // getter + setter
-@AllArgsConstructor
+//@Data // getter + setter
+//@AllArgsConstructor // 생성자
+//@NoArgsConstructor // 빈 생성자
+//@RequiredArgsConstructor final붙은 애들에 대한 생성자를 만들어준다.
+
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
-	private int id;
-	private String username;
-	private String password;
-	private String email;
-//   1
+	private  int id;
+	private  String username;
+	private  String password;
+	private  String email;
+//      
+	
+	@Builder
+	public Member(int id, String username, String password, String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	// (build 패턴 공부하기)
+	// Builder를 사용하면 
+	// 사용하려는 매개변수에 따른 각각의 생성자를 굳이 만들필요가 없다.
+	// 또한 builder()를 사용할 때 순서를 안 지켜도 된다.
+	// ex
+	// 기존 사용방식 -> Member m = new Member((매개변수))
+	// Builder 사용방식 -> Member m = Member.builder().username("ssar").password("1234").build();
+	
+	
 
 
 }
