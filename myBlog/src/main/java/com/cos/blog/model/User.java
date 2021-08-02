c35451174e8e2c33688f59951894d9d64cd9b433
hanjae do <godohanjae@gmail.com>
2021-08-02 11:53:34 +0900

    test

--- myBlog/src/main/java/com/cos/blog/model/User.java
+++ myBlog/src/main/java/com/cos/blog/model/User.java
@@ -28,10 +28,11 @@ import lombok.NoArgsConstructor;
 @Entity // User 클래스가 MYSQL에 테이블이 생성이 된다.
 //@DynamicInsert // insert할 때 null인 필드 제외
 public class User {
-//	.
+//	...
 	@Id // Primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
 	private int id; // 시퀀스, auto_increment
+	private int pw;
 	
 	@Column(nullable = false, length = 30, unique = true) // unique 중복X
 	private String username; // 아이디
